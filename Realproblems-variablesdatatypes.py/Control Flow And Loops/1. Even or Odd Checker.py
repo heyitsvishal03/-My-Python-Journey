e9dac1594c3a9d88b775afb3f7ae87ee33e1fa33
heyitsvishal03 <vishalku3003@gmail.com>
2025-07-29 22:34:19 +0530

    practice

---
+++ Realproblems-variablesdatatypes.py/Control Flow And Loops/1. Even or Odd Checker.py
@@ -0,0 +1,9 @@
+# 1. Even or Odd Checker
+
+n = int(input("Enter your no for checking is it 'even' or 'odd' "))
+if (n % 2 == 0):
+    print("You number is even no")
+elif(n % 2 != 0) :
+    print("Your number is odd number")
+else :
+    print("Invalid")
